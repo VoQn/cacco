c6b24e611fe5c8b70322f7d58ecd87f5cdb031ac
Kazuhiro Mizushima <voqn.tyrant@gmail.com>
2017-05-22 07:54:00 +0900

    Added Document Comment

--- src/Cacco/Lexer.hs
+++ src/Cacco/Lexer.hs
@@ -20,18 +20,21 @@ import           Text.Megaparsec.ByteString (Parser)
 import           Text.Megaparsec.Char       (oneOf, spaceChar)
 import qualified Text.Megaparsec.Lexer      as L
 
--- |Skipping space characters and comments
+-- |Skipping space characters and comments.
 spaceConsumer :: Parser ()
 spaceConsumer = L.space skipSpace skipLineComment skipBlockComment
-  where
-    skipSpace :: Parser ()
-    skipSpace = void spaceChar
+    where
+      -- |Ignore a space-characters.
+      skipSpace :: Parser ()
+      skipSpace = void spaceChar
 
-    skipLineComment :: Parser ()
-    skipLineComment = L.skipLineComment ";;"
+      -- |Ignore single line comment.
+      skipLineComment :: Parser ()
+      skipLineComment = L.skipLineComment ";;"
 
-    skipBlockComment :: Parser ()
-    skipBlockComment = L.skipBlockCommentNested ";/" "/;"
+      -- |Ignore nested block comment.
+      skipBlockComment :: Parser ()
+      skipBlockComment = L.skipBlockCommentNested ";/" "/;"
 
 -- |Make parser to ignore any space and comment expressions
 lexeme :: Parser a -> Parser a
@@ -60,24 +63,28 @@ withPositionRange parser = do
   end   <- getPosition
   return (value, (begin, end))
 
-hexadecimal :: Parser Integer
-hexadecimal = char 'x' >> L.hexadecimal
-
-octal :: Parser Integer
-octal = char 'o' >> L.octal
-
-binary :: Parser Integer
-binary = char 'b' >> readBin <$> some (oneOf "01")
-  where
-    readBin :: String -> Integer
-    readBin = fromIntegral . foldl' (\acc x -> acc * 2 + digitToInt x) 0
-
 -- |Parse a integer number.
 integer :: Parser (Integer, (SourcePos, SourcePos))
 integer = withPositionRange $ try positionalNotation <|> L.integer
   where
+    -- |Parse a integer with prefix for positional notation.
     positionalNotation :: Parser Integer
     positionalNotation = char '0' >> choice [hexadecimal, octal, binary]
+
+    -- |Parse a hexadecimal integer with prefix 'x' (e.g. xFA901)
+    hexadecimal :: Parser Integer
+    hexadecimal = char 'x' >> L.hexadecimal
+
+    -- |Parse a octal integer with prefix 'o' (e.g. o080)
+    octal :: Parser Integer
+    octal = char 'o' >> L.octal
+
+    -- |Parse a binary integer with prefix 'b' (e.g. b101010)
+    binary :: Parser Integer
+    binary = char 'b' >> readBin <$> some (oneOf "01")
+
+    readBin :: String -> Integer
+    readBin = fromIntegral . foldl' (\acc x -> acc * 2 + digitToInt x) 0
 
 -- |Parse a floating point number.
 decimal :: Parser (Scientific, (SourcePos, SourcePos))
