2bb4030d4e454dd8e5b70484b78695927110ce35
Kazuhiro Mizushima <voqn.tyrant@gmail.com>
2017-06-30 13:06:25 +0900

    Use $

--- test/Cacco/ParserSpec.hs
+++ test/Cacco/ParserSpec.hs
@@ -13,31 +13,27 @@ spec_parseExpr :: Spec
 spec_parseExpr = describe "Cacco.Parser" $ do
   let testParse = parseExpr "test"
   it "can parse \"+1.0\"" $
-    testParse "+1.0" `shouldBe` Right (
-      Expr.Decimal (Location "test" 1 1) $ fromFloatDigits (1.0 :: Double))
+    testParse "+1.0" `shouldBe` Right $
+      Expr.Decimal (Location "test" 1 1) $ fromFloatDigits (1.0 :: Double)
 
   it "can parse \"(dec x Integer)\"" $
-    testParse "(dec x Integer)" `shouldBe` Right (
-      Expr.Declare (Location "test" 1 1) "x" (Expr.Atom (Location "test" 1 8) "Integer")
-    )
+    testParse "(dec x Integer)" `shouldBe` Right $
+      Expr.Declare (Location "test" 1 1) "x" $ Expr.Atom (Location "test" 1 8) "Integer"
 
   it "can parse \"(let x 100)\"" $
-    testParse "(let x 100)" `shouldBe` Right (
-      Expr.Let (Location "test" 1 1) "x" (Expr.Integer (Location "test" 1 8) 100)
-    )
+    testParse "(let x 100)" `shouldBe` Right $
+      Expr.Let (Location "test" 1 1) "x" $ Expr.Integer (Location "test" 1 8) 100
 
   it "can parse \"(var x 100)\"" $
-    testParse "(var x 100)" `shouldBe` Right (
-      Expr.Var (Location "test" 1 1) "x" (Expr.Integer (Location "test" 1 8) 100)
-    )
+    testParse "(var x 100)" `shouldBe` Right $
+      Expr.Var (Location "test" 1 1) "x" $ Expr.Integer (Location "test" 1 8) 100
 
   it "can parse \"(set! x 0)\"" $
-    testParse "(set! x 0)" `shouldBe` Right (
-      Expr.Reassign (Location "test" 1 1) "x" (Expr.Integer (Location "test" 1 9) 0)
-    )
+    testParse "(set! x 0)" `shouldBe` Right $
+      Expr.Reassign (Location "test" 1 1) "x" $ Expr.Integer (Location "test" 1 9) 0
 
   it "can parse \"(foo true false\\n undefined 2)\"" $
-    testParse "(foo true false\n undefined \"hello\" 2)" `shouldBe` Right (
+    testParse "(foo true false\n undefined \"hello\" 2)" `shouldBe` Right $
       Expr.Call (Location "test" 1 1) (Expr.Atom (Location "test" 1  2) "foo") [
         Expr.Boolean (Location "test" 1  6) True,
         Expr.Boolean (Location "test" 1 11) False,
@@ -45,14 +41,11 @@ spec_parseExpr = describe "Cacco.Parser" $ do
         Expr.String  (Location "test" 2 12) "hello",
         Expr.Integer (Location "test" 2 20) 2
       ]
-    )
 
   it "can parse expression with ignoreing line comments" $
-    testParse ";; if this comments didn't ignore, this test case was failed.\ntrue" `shouldBe` Right (
+    testParse ";; if this comments didn't ignore, this test case was failed.\ntrue" `shouldBe` Right $
       Expr.Boolean (Location "test" 2 1) True
-    )
 
   it "can parse expression with ignoreing block comments" $
-    testParse ";/ if this comments didn't ignore, this test case was failed. /;\ntrue" `shouldBe` Right (
+    testParse ";/ if this comments didn't ignore, this test case was failed. /;\ntrue" `shouldBe` Right $
       Expr.Boolean (Location "test" 2 1) True
-    )
