461e988660af448fdecde8d4e5a60fc6e2a23d58
Kazuhiro Mizushima <voqn.tyrant@gmail.com>
2018-02-11 23:41:11 +0900

    Added IndexFunctor version AST (but have not used yet)

--- src/Cacco/Ast.hs
+++ src/Cacco/Ast.hs
@@ -1,31 +1,119 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveFoldable       #-}
+{-# LANGUAGE DeriveFunctor        #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE KindSignatures       #-}
+{-# LANGUAGE LambdaCase           #-}
+{-# LANGUAGE PatternSynonyms      #-}
+{-# LANGUAGE PolyKinds            #-}
+{-# LANGUAGE RankNTypes           #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
 
-module Cacco.Ast
-  ( Ast(..)
-  , pureAst
-  ) where
+module Cacco.Ast where
 
-import           Cacco.Literal (Literal)
-import           Data.Functor  ((<$>))
-import           Data.Typeable (Typeable)
+import           Cacco.IxFix      (IxFix (..), IxFunctor (..),
+                                   IxTraversable (..), cata, imapDefault)
+import           Data.Traversable ()
 
-data Ast a
-  -- | Undefined literal
-  = Literal Literal
-  -- | Symbol
-  | Symbol  String
-  -- | Linked List
-  | List    [Ast a]
-  -- | Vector
-  | Vector  [Ast a]
-  -- | Ast with something meta data
-  | With    a (Ast a)
-  deriving (Eq, Ord, Show, Typeable)
+import           Cacco.Literal    (Literal)
 
-pureAst :: Ast a -> Ast a
-pureAst x = case x of
-  (Literal _) -> x
-  (Symbol  _) -> x
-  (With _  y) -> pureAst y
-  (List   xs) -> List $ pureAst <$> xs
-  (Vector xs) -> Vector $ pureAst <$> xs
+data Var
+  = VarId String
+  | ConId String
+  | VarSym String
+  | ConSym String
+  deriving (Eq, Ord, Show)
+
+data AstIx
+  = AstDecl
+  | AstExpr
+  | AstPatt
+  | AstType
+  -- deriving (Eq, Show)
+
+type AstDecl = 'AstDecl
+type AstExpr = 'AstExpr
+type AstPatt = 'AstPatt
+type AstType = 'AstType
+
+data AstIxProxy (i :: AstIx) where
+  DeclProxy :: AstIxProxy AstDecl
+  ExprProxy :: AstIxProxy AstExpr
+  PattProxy :: AstIxProxy AstPatt
+  TypeProxy :: AstIxProxy AstType
+
+data AstF (f :: AstIx -> *) (i :: AstIx) where
+  VarF :: Var -> AstIxProxy i -> AstF f i
+  LitF :: Literal -> AstF f AstExpr
+  LisF :: [f AstExpr] -> AstF f AstExpr
+  AppF :: f i -> [f i] -> AstF f i
+  LamF :: [f AstPatt] -> f AstExpr -> AstF f AstExpr
+  HoleF :: AstF f AstPatt
+
+type Ast = IxFix AstF
+
+pattern Var :: forall (i :: AstIx).
+               Var          -- ^ variable identifier
+            -> AstIxProxy i -- ^ index proxy
+            -> Ast i
+pattern Var v i = In (VarF v i)
+
+pattern Lit :: forall (i :: AstIx). ()
+            => i ~ AstExpr
+            => Literal   -- ^ concrete literal
+            -> Ast i
+pattern Lit l = In (LitF l)
+
+pattern Lis :: forall (i :: AstIx). ()
+            => i ~ AstExpr
+            => [Ast AstExpr]
+            -> Ast i
+pattern Lis elements = In (LisF elements)
+
+pattern App :: forall (i :: AstIx).
+               Ast i   -- ^ function
+            -> [Ast i] -- ^ arguments
+            -> Ast i
+pattern App fn args = In (AppF fn args)
+
+pattern Lam :: forall (i :: AstIx).()
+            => i ~ AstExpr
+            => [Ast AstPatt] -- ^ parameters pattern
+            -> Ast AstExpr   -- ^ body expression
+            -> Ast i
+pattern Lam patterns expr = In (LamF patterns expr)
+
+pattern Hole :: forall (i :: AstIx).()
+             => i ~ AstPatt
+             => Ast i
+pattern Hole = In HoleF
+
+newtype AnnAstF an fn ix = Ann { unAnn :: (an, AstF fn ix) }
+
+instance IxFunctor AstF where
+  imap = imapDefault
+
+instance IxFunctor (AnnAstF x) where
+  imap = imapDefault
+
+instance IxTraversable AstF where
+  itraverse _ (VarF v i) = VarF <$> pure v <*> pure i
+  itraverse _ (LitF l)   = LitF <$> pure l
+  itraverse _ HoleF      = pure HoleF
+
+  itraverse f ast = case ast of
+      LisF elems   -> LisF <$> mapList f elems
+      AppF fn args -> AppF <$> f fn <*> mapList f args
+      LamF ps expr -> LamF <$> mapList f ps <*> f expr
+    where
+      mapList fn xs = sequenceA $ fn <$> xs
+
+instance IxTraversable (AnnAstF x) where
+  itraverse f (Ann (x, t)) = Ann . (,) x <$> itraverse f t
+
+removeAnn :: IxFix (AnnAstF a) i -> IxFix AstF i
+removeAnn = cata $ In . snd . unAnn
---
+++ src/Cacco/IxFix.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveFoldable       #-}
+{-# LANGUAGE DeriveFunctor        #-}
+{-# LANGUAGE DeriveTraversable    #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE PatternSynonyms      #-}
+{-# LANGUAGE PolyKinds            #-}
+{-# LANGUAGE RankNTypes           #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Cacco.IxFix where
+
+import           Control.Monad
+
+-- | Natural transformation
+type f ~> g = forall i. f i -> g i
+infixr 5 ~>
+
+-- | Constant on the left
+type f .~> g = forall i. f -> g i
+infixr 5 .~>
+
+-- | Constant on the right
+type f ~>. g = forall i. f i -> g
+infixr 5 ~>.
+
+-- | synonym type 'K'onst functor
+newtype K a b = K { unK:: a }
+  deriving (Eq, Show, Functor, Foldable, Traversable)
+
+-- | synonym type 'I'dentity functor
+newtype I a = I { unI :: a }
+  deriving (Eq, Show, Functor, Foldable, Traversable)
+
+instance Applicative I where
+  pure = I
+  I f <*> I a = I (f a)
+
+instance Monoid a => Applicative (K a) where
+  pure _ = K mempty
+  K a <*> K a' = K (mappend a a')
+
+-- | Indexed functor
+class IxFunctor (f :: (k -> *) -> (k -> *)) where
+  -- | map with taking over each index
+  imap :: (a ~> b)     -- ^ convert function switch functor wrap
+       -> (f a ~> f b)
+
+-- | Indexed traversable
+class IxFunctor t => IxTraversable t where
+  -- | traverse with taking over each index
+  itraverse :: Applicative f
+            => (forall i. a i -> f (b i))     -- ^ lifting functor
+            -> (forall i. t a i -> f (t b i))
+
+-- | Indexed foldable
+class IxFoldable t where
+  iFoldMap :: Monoid m
+           => (a ~>. m)
+           -> (t a ~>. m)
+
+imapDefault :: IxTraversable t
+            => (a ~> b)
+            -> (t a ~> t b)
+imapDefault f = unI . itraverse (I . f)
+
+iFoldMapDefault :: (IxTraversable t, Monoid m)
+                => (a ~>. m)
+                -> (t a ~>. m)
+iFoldMapDefault f = unK . itraverse (K . f)
+
+-- | Indexed fix-point
+newtype IxFix f i = In { out :: f (IxFix f) i }
+
+deriving instance Show (f (IxFix f) t) => Show (IxFix f t)
+deriving instance Eq (f (IxFix f) t) => Eq (IxFix f t)
+deriving instance Ord (f (IxFix f) t) => Ord (IxFix f t)
+
+-- | folding returning indexed type.
+cata :: IxFunctor f
+     => (f a ~> a)
+     -> (IxFix f ~> a)
+cata phi = phi . imap (cata phi) . out
+
+-- | monadic folding returning indexed type.
+cataM :: (Monad m, IxTraversable t)
+      => (forall i. t a i -> m (a i))
+      -> (forall i. IxFix t i -> m (a i))
+cataM phi = phi <=< itraverse (cataM phi) . out
+
+-- | folding returning constant type.
+cata' :: IxFunctor f
+      => (f (K a) ~>. a)
+      -> (IxFix f ~>. a)
+cata' phi = unK . cata (K . phi)
+
+-- | monadic folding returning constant type.
+cataM' :: (Monad m, IxTraversable t)
+       => (t (K a) ~>. m a)
+       -> (IxFix t ~>. m a)
+cataM' phi = phi <=< itraverse (fmap K . cataM' phi) . out
+
